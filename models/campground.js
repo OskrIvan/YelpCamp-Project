a7669bd5f83f9eb1e7ec08600cdee3d0773edf3b
Oscar Paz <oskrivan.arq@gmail.com>
2023-03-09 12:02:32 -0600

    Adding all files from YelpCamp app

---
+++ models/campground.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CampgroundSchema = new Schema({
+    title: String,
+    price: Number,
+    description: String,
+    location: String,
+    image: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+});
+
+module.exports = mongoose.model('Campground', CampgroundSchema); 
