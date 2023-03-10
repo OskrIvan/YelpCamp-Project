51a66c1a6dff73bec5933e9dc552869097dd4db7
Oscar Paz <oskrivan.arq@gmail.com>
2023-03-10 11:49:52 -0600

    updated all ejs files

--- app.js
+++ app.js
@@ -55,10 +55,6 @@ const validateReview = (req, res, next) => {
 }
 
 app.get('/', (req, res) => {
-    res.render('Home')
-});
-
-app.get('/', (req, res) => {
     res.render('home')
 });
 
@@ -108,6 +104,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
 }));
+
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}))
 
 app.all('*', (req, res, next) => {
     next(new (ExpressError('Page not found', 404)))
--- schemas.js
+++ schemas.js
@@ -1,9 +1,10 @@
 const Joi = require('joi');
+const { number } = require('joi');
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
         title: Joi.string().required(),
-        price: Joi.number().required(),
+        price: Joi.number().required().min(0),
         image: Joi.string().required(),
         location: Joi.string().required(),
         description: Joi.string().required()
@@ -12,7 +13,7 @@ module.exports.campgroundSchema = Joi.object({
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
-        rating: Joi.number().required(),
-        body: Joi.string().required().min(1).max(5)
+        rating: Joi.number().required().min(1).max(5),
+        body: Joi.string().required()
     }).required()
-});
+})
