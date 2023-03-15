0f2e3c429c000e1ffabf183f768c0b8a09febd1f
Oscar Paz <oskrivan.arq@gmail.com>
2023-03-15 11:19:17 -0600

    adding the reviews route

---
+++ routes/reviews.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const catchAsync = require('../Utilities/catchAsync');
+const ExpressError = require('../Utilities/ExpressError');
+const Campground = require('../models/campground');
+const Review = require('../models/review');
+const { campgroundSchema, reviewSchema } = require('../schemas.js');
+
+router.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    const review = new Review(req.body.review);
+    campground.reviews.push(review);
+    await review.save();
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+}));
+
+router.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}));
+
+module.exports = router;
