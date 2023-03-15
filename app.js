a3f316d322e4e3210c7a4e2ddfda29e7411db0c6
Oscar Paz <oskrivan.arq@gmail.com>
2023-03-15 11:16:28 -0600

    adding the routes file

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const Review = require('./models/review');
 const { nextTick } = require('process');
 const catchAsync = require('./Utilities/catchAsync');
 const ExpressError = require('./Utilities/ExpressError');
-
+const campgrounds = require('./routes/campgrounds');
 
 
 
@@ -54,47 +54,12 @@ const validateReview = (req, res, next) => {
     }
 }
 
+app.use("/campgrounds", campgrounds);
+
 app.get('/', (req, res) => {
     res.render('home')
 });
 
-app.get('/campgrounds', catchAsync(async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
-}));
-
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-});
-
-app.post('/campgrounds', validateCampground, catchAsync(async (req, res) => {
-    // if (!req.body.campground) throw new ExpressError('Invalid Campground data', 400)
-    const campground = new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-}));
-
-app.get('/campgrounds/:id', catchAsync(async (req, res,) => {
-    const campground = await Campground.findById(req.params.id).populate('reviews')
-    res.render('campgrounds/show', { campground });
-}));
-
-app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', { campground });
-}))
-
-app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`)
-}));
-
-app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
-    const { id } = req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect('/campgrounds');
-}));
 
 app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
