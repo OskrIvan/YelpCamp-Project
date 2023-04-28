8dea7aee55fefbdb623f6abcf71cfd041986bb9e
Oscar Paz <oskrivan.arq@gmail.com>
2023-04-27 19:34:04 -0600

    removed the .populate from controllers>campground as it was causing an error

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -5,7 +5,7 @@ const geocoder = mbxGeocoding({ accessToken: mapBoxToken })
 const { cloudinary } = require("../cloudinary");
 
 module.exports.index = async (req, res) => {
-    const campgrounds = await Campground.find({}).populate('popupText');
+    const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
 };
 
