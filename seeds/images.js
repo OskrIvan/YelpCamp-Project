58a7792c82055bb1ee07221cbae67f2198cf2a61
Oscar Paz <oskrivan.arq@gmail.com>
2023-04-29 22:51:25 -0600

    changed the seeds for the images in order to have 4 different pictures for each campground

--- seeds/images.js
+++ seeds/images.js
@@ -1,126 +1,250 @@
 module.exports = [
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788381/YelpCamp/46418CB5-009A-DFF0-BC93DEEE9944741A_krblnn.jpg",
-        filename: "YelpCamp/46418CB5-009A-DFF0-BC93DEEE9944741A_krblnn.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788381/YelpCamp/46418CB5-009A-DFF0-BC93DEEE9944741A_krblnn.jpg",
+        name: "YelpCamp/46418CB5-009A-DFF0-BC93DEEE9944741A_krblnn.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788365/YelpCamp/smc4_hqdpv1.jpg",
-        filename: "YelpCamp/smc4_hqdpv1.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788365/YelpCamp/smc4_hqdpv1.jpg",
+        name: "YelpCamp/smc4_hqdpv1.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788353/YelpCamp/GettyImages-1046045064_wogkyt.jpg",
-        filename: "YelpCamp/GettyImages-1046045064_wogkyt.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788353/YelpCamp/GettyImages-1046045064_wogkyt.jpg",
+        name: "YelpCamp/GettyImages-1046045064_wogkyt.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788348/YelpCamp/I3umm91EjVtR_x4cfv6.jpg",
-        filename: "YelpCamp/I3umm91EjVtR_x4cfv6.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788348/YelpCamp/I3umm91EjVtR_x4cfv6.jpg",
+        name: "YelpCamp/I3umm91EjVtR_x4cfv6.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788347/YelpCamp/Watchman-Campground-1920x1440_wzwzzy.jpg",
-        filename: "YelpCamp/Watchman-Campground-1920x1440_wzwzzy.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788347/YelpCamp/Watchman-Campground-1920x1440_wzwzzy.jpg",
+        name: "YelpCamp/Watchman-Campground-1920x1440_wzwzzy.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788344/YelpCamp/white-mountain-national-forest-new-hampshire-SCENICCAMP0118-d58ec12ec2d7491fbe5e1c3377b88131_dqzpv7.jpg",
-        filename: "YelpCamp/white-mountain-national-forest-new-hampshire-SCENICCAMP0118-d58ec12ec2d7491fbe5e1c3377b88131_dqzpv7.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788344/YelpCamp/white-mountain-national-forest-new-hampshire-SCENICCAMP0118-d58ec12ec2d7491fbe5e1c3377b88131_dqzpv7.jpg",
+        name: "YelpCamp/white-mountain-national-forest-new-hampshire-SCENICCAMP0118-d58ec12ec2d7491fbe5e1c3377b88131_dqzpv7.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788337/YelpCamp/washington-mount-rainier-national-park-best-campgrounds-washington-cougar-rock-campground-paradise-skyline-trail_av4ugr.jpg",
-        filename: "YelpCamp/washington-mount-rainier-national-park-best-campgrounds-washington-cougar-rock-campground-paradise-skyline-trail_av4ugr.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788337/YelpCamp/washington-mount-rainier-national-park-best-campgrounds-washington-cougar-rock-campground-paradise-skyline-trail_av4ugr.jpg",
+        name: "YelpCamp/washington-mount-rainier-national-park-best-campgrounds-washington-cougar-rock-campground-paradise-skyline-trail_av4ugr.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788332/YelpCamp/tips-for-solo-campers_ki9a9c.jpg",
-        filename: "YelpCamp/tips-for-solo-campers_ki9a9c.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788332/YelpCamp/tips-for-solo-campers_ki9a9c.jpg",
+        name: "YelpCamp/tips-for-solo-campers_ki9a9c.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788331/YelpCamp/Tent_camping_along_the_Sulayr_trail_in_La_Taha__Sierra_Nevada_National_Park__DSCF5147_vkfxud.jpg",
-        filename: "YelpCamp/Tent_camping_along_the_Sulayr_trail_in_La_Taha__Sierra_Nevada_National_Park__DSCF5147_vkfxud.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788331/YelpCamp/Tent_camping_along_the_Sulayr_trail_in_La_Taha__Sierra_Nevada_National_Park__DSCF5147_vkfxud.jpg",
+        name: "YelpCamp/Tent_camping_along_the_Sulayr_trail_in_La_Taha__Sierra_Nevada_National_Park__DSCF5147_vkfxud.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788320/YelpCamp/summer-camp_bonozk.jpg",
-        filename: "YelpCamp/summer-camp_bonozk.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788320/YelpCamp/summer-camp_bonozk.jpg",
+        name: "YelpCamp/summer-camp_bonozk.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788310/YelpCamp/DSC_0796cropped_kp2iox.jpg",
-        filename: "YelpCamp/DSC_0796cropped_kp2iox.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788310/YelpCamp/DSC_0796cropped_kp2iox.jpg",
+        name: "YelpCamp/DSC_0796cropped_kp2iox.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788305/YelpCamp/fseprd923724_qvozeo.jpg",
-        filename: "YelpCamp/fseprd923724_qvozeo.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788305/YelpCamp/fseprd923724_qvozeo.jpg",
+        name: "YelpCamp/fseprd923724_qvozeo.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788290/YelpCamp/Spray-Lakes-West-Camping-in-Alberta-1024x768_gukjyd.jpg",
-        filename: "YelpCamp/Spray-Lakes-West-Camping-in-Alberta-1024x768_gukjyd.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788290/YelpCamp/Spray-Lakes-West-Camping-in-Alberta-1024x768_gukjyd.jpg",
+        name: "YelpCamp/Spray-Lakes-West-Camping-in-Alberta-1024x768_gukjyd.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788288/YelpCamp/fseprd923694_edpszu.jpg",
-        filename: "YelpCamp/fseprd923694_edpszu.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788288/YelpCamp/fseprd923694_edpszu.jpg",
+        name: "YelpCamp/fseprd923694_edpszu.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788281/YelpCamp/IMG_5228_camp_princeton_2000_ehtlcx.jpg",
-        filename: "YelpCamp/IMG_5228_camp_princeton_2000_ehtlcx.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788281/YelpCamp/IMG_5228_camp_princeton_2000_ehtlcx.jpg",
+        name: "YelpCamp/IMG_5228_camp_princeton_2000_ehtlcx.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788273/YelpCamp/Camping-social_oo0h2x.jpg",
-        filename: "YelpCamp/Camping-social_oo0h2x.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788273/YelpCamp/Camping-social_oo0h2x.jpg",
+        name: "YelpCamp/Camping-social_oo0h2x.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788260/YelpCamp/descarga_2_nanohh.jpg",
-        filename: "YelpCamp/descarga_2_nanohh.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788260/YelpCamp/descarga_2_nanohh.jpg",
+        name: "YelpCamp/descarga_2_nanohh.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788259/YelpCamp/seethanadi-nature-camp1_935x518_acf_cropped_boe8zi.jpg",
-        filename: "YelpCamp/seethanadi-nature-camp1_935x518_acf_cropped_boe8zi.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788259/YelpCamp/seethanadi-nature-camp1_935x518_acf_cropped_boe8zi.jpg",
+        name: "YelpCamp/seethanadi-nature-camp1_935x518_acf_cropped_boe8zi.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788257/YelpCamp/descarga_mjqc9j.jpg",
-        filename: "YelpCamp/descarga_mjqc9j.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788257/YelpCamp/descarga_mjqc9j.jpg",
+        name: "YelpCamp/descarga_mjqc9j.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788255/YelpCamp/d90b0286cd769846192fc673f4e0e156_nbridgecampgroundwhittleton819_ou5nvt.jpg",
-        filename: "YelpCamp/d90b0286cd769846192fc673f4e0e156_nbridgecampgroundwhittleton819_ou5nvt.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788255/YelpCamp/d90b0286cd769846192fc673f4e0e156_nbridgecampgroundwhittleton819_ou5nvt.jpg",
+        name: "YelpCamp/d90b0286cd769846192fc673f4e0e156_nbridgecampgroundwhittleton819_ou5nvt.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788252/YelpCamp/running-waters-pa-01_m7udry.jpg",
-        filename: "YelpCamp/running-waters-pa-01_m7udry.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788252/YelpCamp/running-waters-pa-01_m7udry.jpg",
+        name: "YelpCamp/running-waters-pa-01_m7udry.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788248/YelpCamp/Red-Rock-Canyon-Campground_okbcyg.jpg",
-        filename: "YelpCamp/Red-Rock-Canyon-Campground_okbcyg.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788248/YelpCamp/Red-Rock-Canyon-Campground_okbcyg.jpg",
+        name: "YelpCamp/Red-Rock-Canyon-Campground_okbcyg.jpg",
     },
     {
-        url: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788249/YelpCamp/PlanYourTrip_Places_to_Stay_Camgrounds_RV_Parks__istockphoto_pkujiahe_crop_resize_0_bv8gdv.jpg",
-        filename: "YelpCamp/PlanYourTrip_Places_to_Stay_Camgrounds_RV_Parks__istockphoto_pkujiahe_crop_resize_0_bv8gdv.jpg",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788249/YelpCamp/PlanYourTrip_Places_to_Stay_Camgrounds_RV_Parks__istockphoto_pkujiahe_crop_resize_0_bv8gdv.jpg",
+        name: "YelpCamp/PlanYourTrip_Places_to_Stay_Camgrounds_RV_Parks__istockphoto_pkujiahe_crop_resize_0_bv8gdv.jpg",
     },
     {
-        url: "",
-        filename: "",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788244/YelpCamp/primitive-camp-hero_j49eso.jpg",
+        name: "YelpCamp/primitive-camp-hero_j49eso.jpg",
     },
     {
-        url: "",
-        filename: "",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788241/YelpCamp/agur-lake-camp-high-res-124-1_orig_x4pojm.jpg",
+        name: "YelpCamp/agur-lake-camp-high-res-124-1_orig_x4pojm.jpg",
     },
     {
-        url: "",
-        filename: "",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788240/YelpCamp/photo-1487730116645-74489c95b41b_y2ufeq.jpg",
+        name: "YelpCamp/photo-1487730116645-74489c95b41b_y2ufeq.jpg",
     },
     {
-        url: "",
-        filename: "",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788238/YelpCamp/descarga_1_tm2uv5.jpg",
+        name: "YelpCamp/descarga_1_tm2uv5.jpg",
     },
     {
-        url: "",
-        filename: "",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788237/YelpCamp/PHOTO_ICON_CAMPING_FISHEATING_CREEK_Peter_W_Cross_and_Patrick_Farrell__cfc2d3c3-3be5-49f0-9963-8135882ce0a9_prptea.jpg",
+        name: "YelpCamp/PHOTO_ICON_CAMPING_FISHEATING_CREEK_Peter_W_Cross_and_Patrick_Farrell__cfc2d3c3-3be5-49f0-9963-8135882ce0a9_prptea.jpg",
     },
     {
-        url: "",
-        filename: "",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788236/YelpCamp/Over-1800-Campgrounds-In-The-U.S.-And-Canada-Are-Now-Available-On-Booking.com-_we279p.jpg",
+        name: "YelpCamp/Over-1800-Campgrounds-In-The-U.S.-And-Canada-Are-Now-Available-On-Booking.com-_we279p.jpg",
     },
     {
-        url: "",
-        filename: "",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788235/YelpCamp/denali-national-park-alaska-SCENICCAMP0118-da0e67391a1d41b1a348aebce7d32fad_cuyqmd.jpg",
+        name: "YelpCamp/denali-national-park-alaska-SCENICCAMP0118-da0e67391a1d41b1a348aebce7d32fad_cuyqmd.jpg",
     },
     {
-        url: "",
-        filename: "",
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788233/YelpCamp/Camping-campfire-LakeElizabeth-GreatOtwayNationalPark-1920x1124_q8qbcc.jpg",
+        name: "YelpCamp/Camping-campfire-LakeElizabeth-GreatOtwayNationalPark-1920x1124_q8qbcc.jpg",
     },
-]
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788232/YelpCamp/NatureLake_fvsqno.jpg",
+        name: "YelpCamp/NatureLake_fvsqno.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788230/YelpCamp/Organ-Pipe-Cactus-NM-camping_IG-bonitavistaphotography_50f989c3c31d5b1d8623b67a92726880_ibn7am.jpg",
+        name: "YelpCamp/Organ-Pipe-Cactus-NM-camping_IG-bonitavistaphotography_50f989c3c31d5b1d8623b67a92726880_ibn7am.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788226/YelpCamp/canada-canmore-best-campgrounds-lac-des-arc-table_n7fcad.jpg",
+        name: "YelpCamp/canada-canmore-best-campgrounds-lac-des-arc-table_n7fcad.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788222/YelpCamp/OhanaCampground2016_CMeleedy_01_web_fcuegx.jpg",
+        name: "YelpCamp/OhanaCampground2016_CMeleedy_01_web_fcuegx.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788218/YelpCamp/Newport_Campground_Nature_Path_-_Copy_abhrhc.jpg",
+        name: "YelpCamp/Newport_Campground_Nature_Path_-_Copy_abhrhc.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788214/YelpCamp/learn-to-camp-quetico_k3ca4n.jpg",
+        name: "YelpCamp/learn-to-camp-quetico_k3ca4n.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788210/YelpCamp/lake-wenatchee-state-park_HERO_CAMPWA0922-09e49c99f7fd4a3b9238bdfd4475928b_mekagw.jpg",
+        name: "YelpCamp/lake-wenatchee-state-park_HERO_CAMPWA0922-09e49c99f7fd4a3b9238bdfd4475928b_mekagw.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788209/YelpCamp/images_9_lxwzk0.jpg",
+        name: "YelpCamp/images_9_lxwzk0.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788205/YelpCamp/campgrounds-in-california_vs22xf.jpg",
+        name: "YelpCamp/campgrounds-in-california_vs22xf.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788204/YelpCamp/1607141526658-rockclimbing_szipuh.jpg",
+        name: "YelpCamp/1607141526658-rockclimbing_szipuh.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788202/YelpCamp/images_tvkssp.jpg",
+        name: "YelpCamp/images_tvkssp.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788202/YelpCamp/best-camping-in-new-england-768x529_pfgohg.jpg",
+        name: "YelpCamp/best-camping-in-new-england-768x529_pfgohg.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788199/YelpCamp/bentongnature3_j9ie0w.jpg",
+        name: "YelpCamp/bentongnature3_j9ie0w.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788198/YelpCamp/images_8_kmkcbx.jpg",
+        name: "YelpCamp/images_8_kmkcbx.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788198/YelpCamp/campground_image_t9cngt.jpg",
+        name: "YelpCamp/campground_image_t9cngt.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788195/YelpCamp/image_ahxarh.jpg",
+        name: "YelpCamp/image_ahxarh.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788194/YelpCamp/images_7_pqnuuw.jpg",
+        name: "YelpCamp/images_7_pqnuuw.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788191/YelpCamp/image_brbmpr.png",
+        name: "YelpCamp/image_brbmpr.png",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788182/YelpCamp/images_6_nrzgbc.jpg",
+        name: "YelpCamp/images_6_nrzgbc.jpg",
+    },
+    {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788181/YelpCamp/images_5_kemcxz.jpg",
+        name: "YelpCamp/images_5_kemcxz.jpg",
+    },
+     {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788178/YelpCamp/images_4_kmrjg9.jpg",
+        name: "YelpCamp/images_4_kmrjg9.jpg",
+    },
+     {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788178/YelpCamp/images_3_gmcomq.jpg",
+        name: "YelpCamp/images_3_gmcomq.jpg",
+    },
+     {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788175/YelpCamp/acadia-national-park-maine-SCENICCAMP0118-8e810c5462744bcead3f544c2e07b1f1_k0fljs.jpg",
+        name: "YelpCamp/acadia-national-park-maine-SCENICCAMP0118-8e810c5462744bcead3f544c2e07b1f1_k0fljs.jpg",
+    },
+     {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788175/YelpCamp/images_2_kf8iaf.jpg",
+        name: "YelpCamp/images_2_kf8iaf.jpg",
+    },
+     {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788173/YelpCamp/images_1_el62l7.jpg",
+        name: "YelpCamp/images_1_el62l7.jpg",
+    },
+     {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788171/YelpCamp/4d3zi59LoSTp_jxip5t.png",
+        name: "YelpCamp/4d3zi59LoSTp_jxip5t.png",
+    },
+     {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788170/YelpCamp/image_2_pxi93j.jpg",
+        name: "YelpCamp/image_2_pxi93j.jpg",
+    },
+     {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788151/YelpCamp/8_2553_ad5b96e8-3cb8-46b6-9512-733b278fae82_wqw0y1.jpg",
+        name: "YelpCamp/8_2553_ad5b96e8-3cb8-46b6-9512-733b278fae82_wqw0y1.jpg",
+    },
+     {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788150/YelpCamp/960x0_f1wlhh.jpg",
+        name: "YelpCamp/960x0_f1wlhh.jpg",
+    },
+     {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788147/YelpCamp/image_1_jdpsms.jpg",
+        name: "YelpCamp/image_1_jdpsms.jpg",
+    },
+     {
+        adress: "https://res.cloudinary.com/dfedyydhu/image/upload/v1682788145/YelpCamp/5350216d-d397-4b5e-925c-5dc9d9e31d14_image2_9ScenicMountainCampingExperiencesBanner_hsvvjm.jpg",
+        name: "YelpCamp/5350216d-d397-4b5e-925c-5dc9d9e31d14_image2_9ScenicMountainCampingExperiencesBanner_hsvvjm.jpg",
+    },
+];
--- seeds/index.js
+++ seeds/index.js
@@ -3,7 +3,8 @@ const cities = require('./cities');
 const axios = require('axios');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
-const { campgroundImages } = require('./images');
+const images = require('./images');
+const { address, file } = require('./pictures');
 
 main().catch(err => console.log(err));
 
@@ -20,6 +21,7 @@ db.once("open", () => {
 });
 
 const sample = array => array[Math.floor(Math.random() * array.length)];
+
 
 // to feed the image with random images from my unsplash collection.
 // this has been replaced with the database from claudinary. 
@@ -42,10 +44,10 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 200; i++) {
         const random1003 = Math.floor(Math.random() * 1003);
-        const randomImage1 = Math.floor(Math.random() * 50);
-        const randomImage2 = Math.floor(Math.random() * 50);
-        const randomImage3 = Math.floor(Math.random() * 50);
-        const randomImage4 = Math.floor(Math.random() * 50);
+        const randomImage1 = Math.floor(Math.random() * 62);
+        const randomImage2 = Math.floor(Math.random() * 62);
+        const randomImage3 = Math.floor(Math.random() * 62);
+        const randomImage4 = Math.floor(Math.random() * 62);
         const price = Math.floor(Math.random() * 30) + 10;
         const camp = new Campground({
             author: '643dbc0edfbee222f4612c8d',
@@ -62,20 +64,20 @@ const seedDB = async () => {
             },
             images: [
                 {
-                    url: `${images[randomImage1].url}`,
-                    filename: `${images[randomImage1].filename}`,
+                    url: `https://res.cloudinary.com/dfedyydhu/image/upload/v1682788173/${images[randomImage1].name}`,
+                    filename: images[randomImage1].name
                 },
                 {
-                    url: `${images[randomImage2].url}`,
-                    filename: `${images[randomImage2].filename}`,
+                    url: `https://res.cloudinary.com/dfedyydhu/image/upload/v1682788173/${images[randomImage2].name}`,
+                    filename: images[randomImage2].name
                 },
                 {
-                    url: `${images[randomImage3].url}`,
-                    filename: `${images[randomImage3].filename}`,
+                    url: `https://res.cloudinary.com/dfedyydhu/image/upload/v1682788173/${images[randomImage3].name}`,
+                    filename: images[randomImage3].name
                 },
                 {
-                    url: `${images[randomImage4].url}`,
-                    filename: `${images[randomImage4].filename}`,
+                    url: `https://res.cloudinary.com/dfedyydhu/image/upload/v1682788173/${images[randomImage4].name}`,
+                    filename: images[randomImage4].name
                 }
             ]
         })
@@ -83,7 +85,7 @@ const seedDB = async () => {
     }
 }
 
-
+        
 
 seedDB().then(() => {
     mongoose.connection.close();
